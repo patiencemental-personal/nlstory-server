58d037f51f8b14d163542a074fd070baac534fef
patiencemental <ejsvk3284@kakao.com>
2023-02-13 04:15:24 +0900

    cors 설정 오타 수정

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080
 const app = express();
 
 const corsOption = {
-  origin: config.cors.allowedOrigin,
+  origin: config.cors.allowedOrigins,
   credentials: process.env.NODE_ENV === 'production',
   optionsSuccessStatus: 200,
 };
