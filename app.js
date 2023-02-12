26f74928b8f21592b38e6c440902957a78d9f427
patiencemental <ejsvk3284@kakao.com>
2023-02-12 20:11:15 +0900

    cors 의존성 추가

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-import keywordNoteRouter from './router/keywordNote.js';
+import cors from 'cors';
 import express from 'express';
+import keywordNoteRouter from './router/keywordNote.js';
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/keywordNote', keywordNoteRouter);
